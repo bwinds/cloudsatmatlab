0b4bd8c526abcd830e94b440d46e4b936ddbab37
Ethan <ethan-nelson@users.noreply.github.com>
2014-11-14 18:35:13 -0600

    updating errors

--- +cloudsat/io.m
+++ +cloudsat/io.m
@@ -20,25 +20,35 @@ global fid swid
 
 switch action
     case {'open','o'};
-        if nargin ~= 3;
+        if nargin < 3;
             error('Please specify both the file name and algorithm name to open a file.');
+        elseif nargin > 3;
+            error('You have specified too many inputs. Check syntax of command.');
         end;
         fid = matlab.io.hdfeos.sw.open(varargin{1});
         swid = matlab.io.hdfeos.sw.attach(fid, varargin{2});
         field = 'Opened file successfully.';
     case {'close','c'};
+        if nargin > 1;
+            error('Only specify the command `close` to close a file.');
+        end;
         matlab.io.hdfeos.sw.detach(swid);
         matlab.io.hdfeos.sw.close(fid);
         field = 'Closed file successfully.';
         clear fid swid;
     case {'read','r'};
+        if nargin < 2;
+            error('Please specify both the `read` command and the product name.');
+        elseif nargin > 2;
+            error('You have specified too many inputs. Check syntax of command.');
+        end;
         if ~any(strfind(varargin{1},'.'));
             field = double(matlab.io.hdfeos.sw.readField(swid, varargin{1}));
         else;
             field = double(matlab.io.hdfeos.sw.readAttr(swid, varargin{1}));
         end;
     otherwise;
-        error('Please specify what action to perform: open, close, or read.');
+        error('Please specify what action to perform first: `open`, `close`, or `read`.');
 end;
 
 end
